e2831853e193b67aa11fb8a80590bfff00e63e1c
palandarus <klarkken@yandex.ru>
2020-12-02 11:38:32 +0300

    lesson 5 initialization 4 complete
    Signed-off-by: palandarus <klarkken@yandex.ru>

---
+++ spring-lesson-5/src/main/java/com/geekbrains/spring/lesson5/controllers/utils/ErrorHandlerController.java
@@ -0,0 +1,14 @@
+package com.geekbrains.spring.lesson5.controllers.utils;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+
+//    @ExceptionHandler(value = ResourceNotFoundException.class)
+//    @ResponseBody
+//    public String heightError(ResourceNotFoundException ex) {
+//        return ex.getMessage();
+//    }
+
+}
